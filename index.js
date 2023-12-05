bec696940718b1679dc6d108b67a3d4672472278
iamaryanyadavv <aryanyadav1601@gmail.com>
2023-12-05 23:43:59 +0530

    trying the same thing again

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors({
 app.use(express.json()); // Middleware for parsing JSON bodies
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://ashokacoin.netlify.app/');
+    res.header('Access-Control-Allow-Origin', 'https://ashokacoin.netlify.app');
     res.header('Access-Control-Allow-Credentials', 'true');
     next();
 });
